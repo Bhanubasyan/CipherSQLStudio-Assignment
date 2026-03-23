d76d4e342befd948e779562b6c6ca45156e677c9
bhanu1012 <bhanubasyan@gmail.com>
2026-03-23 14:46:04 +0530

    fix backend problem

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,17 @@ require("./routes/assignmentRoutes");
 const app = express();
 
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // 👈 preflight handle
+  }
+
+  next();
+});
 
 app.use(express.json());
 
