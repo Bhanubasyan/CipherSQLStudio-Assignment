b21f1039464b79e7f096044a29eeac7a0d5bbec2
bhanu1012 <bhanubasyan@gmail.com>
2026-03-23 15:01:50 +0530

    fix backend bugs

--- backend/server.js
+++ backend/server.js
@@ -4,32 +4,28 @@ const cors = require("cors");
 
 const queryRoutes = require("./routes/queryRoutes");
 const hintRoutes = require("./routes/hintRoutes");
-const assignmentRoutes =
-require("./routes/assignmentRoutes");
+const assignmentRoutes = require("./routes/assignmentRoutes");
 
 const app = express();
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+app.use(cors());
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // 👈 preflight handle
-  }
-
-  next();
-});
 
 app.use(express.json());
 
-app.use("/api",assignmentRoutes);
+// routes
+app.use("/api", assignmentRoutes);
 app.use("/api", queryRoutes);
 app.use("/api", hintRoutes);
+
 app.get("/", (req, res) => {
-  res.send("CipherSQLStudio Backend is running ");
+  res.send("CipherSQLStudio Backend is running");
 });
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+//FIX PORT 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
